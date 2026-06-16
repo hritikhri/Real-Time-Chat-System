eac9a2bc26fee93328c3802e6190b45b6fbfd367
Hritik <hritik.dev.04@gmail.com>
2026-06-16 08:46:12 +0530

    login authenticator

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ const corsOptions = {
   },
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
+  // allowedHeaders: ["Content-Type", "Authorization", "token"],
 };
 
 // Middlewares
